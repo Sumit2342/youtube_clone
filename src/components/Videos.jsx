6cbdb1948093d6b2285c6c40400212384b71ff02
Sumit2342 <sumitsoreng2000@gmail.com>
2024-12-26 20:53:34 +0530

    adding all commits once

---
+++ src/components/Videos.jsx
@@ -0,0 +1,23 @@
+import { Stack, Box } from "@mui/system";
+import { ChannelCard, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return "Loading....";
+  return (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard ChannelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
